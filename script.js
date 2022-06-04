48fc2fab29a52dab847c0f836074e7e4045d3fc1
Guilherme Garcia <garciaguig@gmail.com>
2022-06-04 18:19:39 -0300

    Conclusão do Requisito 5

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const balls = document.getElementsByClassName('ball');
+const guessColor = document.querySelector('#rgb-color');
+const tryAnswer = document.querySelector('#answer');
 
 function generateRandomColor() {
   const red = Math.floor(Math.random() * 255);
   const blue = Math.floor(Math.random() * 255);
   const green = Math.floor(Math.random() * 255);
-  const randomColor = `rgb(${red} , ${blue} , ${green})`;
+  const randomColor = `rgb(${red}, ${blue}, ${green})`;
   return randomColor;
 }
 
@@ -14,4 +16,26 @@ function setBallsColors() {
   }
 }
 
+function selectRandomBall() {
+  const randomBall = balls[Math.floor(Math.random() * (balls.length - 1))];
+  const randomBallBgColor = randomBall.style.backgroundColor;
+  return randomBallBgColor;
+}
+
+function activeGameLogic() {
+  const randomBallBgColor = selectRandomBall();
+  const onlyNumbers = randomBallBgColor.replace('rgb', '');
+  guessColor.innerHTML = onlyNumbers;
+  for (let i = 0; i < balls.length; i += 1) {
+    balls[i].addEventListener('click', (event) => {
+      if (event.target.style.backgroundColor === randomBallBgColor) {
+        tryAnswer.innerHTML = 'Acertou!';
+      } else {
+        tryAnswer.innerHTML = 'Errou! Tente novamente!';
+      }
+    });
+  }
+}
+
 window.addEventListener('load', setBallsColors);
+window.addEventListener('load', activeGameLogic);
