c621dd4cc9304ebf0056fa3dbd1ccfd7395914fa
Guilherme Garcia <garciaguig@gmail.com>
2022-06-04 19:02:18 -0300

    Conclusão do Requisito 7 (Bônus)

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ const balls = document.getElementsByClassName('ball');
 const guessColor = document.querySelector('#rgb-color');
 const tryAnswer = document.querySelector('#answer');
 const resetBtn = document.querySelector('#reset-game');
+let scoreValue = 0;
+const score = document.querySelector('#score');
+
+function setScoreValue() {
+  score.innerHTML = `Placar: ${scoreValue}`;
+}
 
 function generateRandomColor() {
   const red = Math.floor(Math.random() * 255);
@@ -23,19 +29,25 @@ function selectRandomBall() {
   return randomBallBgColor;
 }
 
-function activeGameLogic() {
-  const randomBallBgColor = selectRandomBall();
-  const onlyNumbers = randomBallBgColor.replace('rgb', '');
-  guessColor.innerHTML = onlyNumbers;
+function clickLogic(x) {
   for (let i = 0; i < balls.length; i += 1) {
     balls[i].addEventListener('click', (event) => {
-      if (event.target.style.backgroundColor === randomBallBgColor) {
+      if (event.target.style.backgroundColor === x) {
         tryAnswer.innerHTML = 'Acertou!';
+        scoreValue += 3;
+        setScoreValue();
       } else {
         tryAnswer.innerHTML = 'Errou! Tente novamente!';
       }
     });
   }
+}
+
+function activeGameLogic() {
+  const randomBallBgColor = selectRandomBall();
+  const onlyNumbers = randomBallBgColor.replace('rgb', '');
+  guessColor.innerHTML = onlyNumbers;
+  clickLogic(randomBallBgColor);
 }
 
 function resetGame() {
@@ -46,4 +58,5 @@ function resetGame() {
 
 window.addEventListener('load', setBallsColors);
 window.addEventListener('load', activeGameLogic);
+window.addEventListener('load', setScoreValue);
 resetBtn.addEventListener('click', resetGame);
